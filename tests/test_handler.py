8bd9279bfdd654b777e1e04a422584ba008d7e22
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-07-30 02:38:20 -0300

    More tests.

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -13,8 +13,66 @@ class HomeHandler(RequestHandler):
     def get(self, **kwargs):
         self.response.out.write('home sweet home')
 
+
+class MethodsHandler(HomeHandler):
+    def post(self, **kwargs):
+        self.response.out.write('home sweet home - POST')
+
+    def put(self, **kwargs):
+        self.response.out.write('home sweet home - PUT')
+
+    def delete(self, **kwargs):
+        self.response.out.write('home sweet home - DELETE')
+
+    def head(self, **kwargs):
+        self.response.out.write('home sweet home - HEAD')
+
+    def trace(self, **kwargs):
+        self.response.out.write('home sweet home - TRACE')
+
+    def options(self, **kwargs):
+        self.response.out.write('home sweet home - OPTIONS')
+
+
+class BrokenHandler(RequestHandler):
+    def get(self, **kwargs):
+        raise ValueError('booo!')
+
+    def handle_exception(self, e, debug):
+        # This is really broken.
+        raise
+
+
+class BrokenButFixedHandler(BrokenHandler):
+    def handle_exception(self, exception, debug_mode):
+        # Let's fix it.
+        self.response.set_status(200)
+        self.response.out.write('that was close!')
+
+
+class Handle404(RequestHandler):
+    def get(self, **kwargs):
+        self.response.out.write('404 custom handler')
+        self.response.set_status(404)
+
+
+class Handle405(RequestHandler):
+    def get(self, **kwargs):
+        #self.response.out.write('405 custom handler')
+        self.response.set_status(405)
+
+
+class Handle500(RequestHandler):
+    def get(self, **kwargs):
+        self.response.out.write('500 custom handler')
+        self.response.set_status(500)
+
+
 app = WSGIApplication([
-    ('/', HomeHandler, 'home'),
+    ('/', HomeHandler),
+    ('/methods', MethodsHandler),
+    ('/broken', BrokenHandler),
+    ('/broken-but-fixed', BrokenButFixedHandler),
 ], debug=False)
 
 
@@ -34,3 +92,62 @@ class TestHandler(unittest.TestCase):
         test_app = TestApp(app)
         res = test_app.put('/', status=405)
         self.assertEqual(res.status, '405 Method Not Allowed')
+
+    def test_500(self):
+        test_app = TestApp(app)
+        res = test_app.get('/broken', status=500)
+        self.assertEqual(res.status, '500 Internal Server Error')
+
+    def test_500_but_fixed(self):
+        test_app = TestApp(app)
+        res = test_app.get('/broken-but-fixed')
+        self.assertEqual(res.status, '200 OK')
+        self.assertEqual(res.body, 'that was close!')
+
+    def test_custom_error_handlers(self):
+        app = WSGIApplication([
+            ('/', HomeHandler),
+            ('/broken', BrokenHandler),
+        ], debug=False)
+
+        app.error_handlers = {
+            404: Handle404,
+            405: Handle405,
+            500: Handle500,
+        }
+
+        test_app = TestApp(app)
+
+        res = test_app.get('/nowhere', status=404)
+        self.assertEqual(res.status, '404 Not Found')
+        self.assertEqual(res.body, '404 custom handler')
+
+        res = test_app.put('/broken', status=405)
+        self.assertEqual(res.status, '405 Method Not Allowed')
+        # body can't be set for 405?
+        #self.assertEqual(res.body, '405 custom handler')
+
+        res = test_app.get('/broken', status=500)
+        self.assertEqual(res.status, '500 Internal Server Error')
+        self.assertEqual(res.body, '500 custom handler')
+
+    def test_methods(self):
+        """Can't test HEAD, OPTIONS and TRACE with webtest."""
+        test_app = TestApp(app)
+
+        res = test_app.get('/methods')
+        self.assertEqual(res.status, '200 OK')
+        self.assertEqual(res.body, 'home sweet home')
+
+        res = test_app.post('/methods')
+        self.assertEqual(res.status, '200 OK')
+        self.assertEqual(res.body, 'home sweet home - POST')
+
+        res = test_app.put('/methods')
+        self.assertEqual(res.status, '200 OK')
+        self.assertEqual(res.body, 'home sweet home - PUT')
+
+        res = test_app.delete('/methods')
+        self.assertEqual(res.status, '200 OK')
+        self.assertEqual(res.body, 'home sweet home - DELETE')
+
