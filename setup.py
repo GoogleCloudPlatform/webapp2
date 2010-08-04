74206676dae3ca082987566aaaf2995a0100e176
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-08-04 10:40:54 -0300

    Updated README.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name = 'webapp2',
-    version = '0.1',
+    version = '0.2',
     license = 'Apache Software License',
     url = 'http://www.tipfy.org/',
     description = "Taking Google App Engine's webapp to the next level!",
