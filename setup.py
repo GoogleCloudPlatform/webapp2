daac5a4960afe2082be2095d3b49c8b89abb0fe2
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2010-08-17 17:05:26 -0300

    Preparing 0.5.1.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name = 'webapp2',
-    version = '0.5',
+    version = '0.5.1',
     license = 'Apache Software License',
     url = 'http://www.tipfy.org/',
     description = "Taking Google App Engine's webapp to the next level!",
