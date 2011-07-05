3c3da2c30ea86a08f2b6f9ffdf75fbe026cb7271
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-07-05 06:20:41 -0300

    Added a pepper to security functions.

--- webapp2_extras/security.py
+++ webapp2_extras/security.py
@@ -9,53 +9,55 @@
     :copyright: (c) 2010 by the Werkzeug Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+import binascii
 import hashlib
 import hmac
 import os
-import random
-import string
 
-SALT_CHARS = string.letters + string.digits
-_SYS_RNG = random.SystemRandom()
+import webapp2
 
 
-def create_token(length=32):
-    """Generates a random string with the specified length.
+def create_token(bit_strength=64, decimal=False):
+    """Generates a random string with the specified bit strength.
 
-    This uses ``os.urandom``, which is suitable for cryptographic use.
-
-    See: http://security.stackexchange.com/questions/3936/is-a-rand-from-dev-urandom-secure-for-a-login-key/3939#3939
-
-    :param length:
-        Length of the string to be returned.
+    :param bit_strength:
+        Bit strength. Must be a multiple of 8.
+    :param decimal:
+        If True, a decimal representation is returned, otherwise an
+        hexadecimal representation is returned.
     :returns:
-        A random string with the specified length.
-
-    This function was ported and adapted from `Werkzeug`_.
+        A random string with the specified bit strength.
     """
-    if length <= 0:
+    if bit_strength % 8 or bit_strength <= 0:
         raise ValueError(
-            'This function expects a positive length, got %r.' % length)
+            'This function expects a bit strength, got %r.' % bit_strength)
 
-    return ''.join(_SYS_RNG.choice(SALT_CHARS) for _ in xrange(length))
+    value = binascii.b2a_hex(os.urandom(bit_strength / 8))
+    if decimal:
+        value = bytes(int(value, 16))
+
+    return value
 
 
-def create_password_hash(password, method='sha1', salt_length=32):
+def create_password_hash(password, method='sha1', bit_strength=64,
+                         pepper=None):
     """Hashes a password.
 
-    The format of the string returned includes the method
-    that was used so that :func:`check_password_hash` can check the hash.
+    The format of the string returned includes the method that was used
+    so that :func:`check_password_hash` can check the hash.
 
     This method can **not** generate unsalted passwords but it is possible
-    to set the method to plain to enforce plaintext passwords.  If a salt
+    to set the method to plain to enforce plaintext passwords. If a salt
     is used, hmac is used internally to salt the password.
 
     :param password:
         The password to hash.
     :param method:
         The hash method to use (``'md5'`` or ``'sha1'``).
-    :param salt_length:
-        Length of the salt to be created.
+    :param bit_strength:
+        Bit strength of the salt to be created. Must be a multiple of 8.
+    :param pepper:
+        A secret constant stored in the application code.
     :returns:
         A formatted hashed string that looks like this::
 
@@ -63,15 +65,15 @@ def create_password_hash(password, method='sha1', salt_length=32):
 
     This function was ported and adapted from `Werkzeug`_.
     """
-    salt = method != 'plain' and create_token(salt_length) or ''
-    hashval = hash_password(password, method, salt)
+    salt = method != 'plain' and create_token(bit_strength) or ''
+    hashval = hash_password(password, method, salt, pepper)
     if hashval is None:
         raise TypeError('Invalid method %r.' % method)
 
     return '%s$%s$%s' % (hashval, method, salt)
 
 
-def check_password_hash(password, pwhash):
+def check_password_hash(password, pwhash, pepper=None):
     """Checks a password against a given salted and hashed password value.
 
     In order to support unsalted legacy passwords this method supports
@@ -81,6 +83,8 @@ def check_password_hash(password, pwhash):
         The plaintext password to compare against the hash.
     :param pwhash:
         A hashed string like returned by :func:`create_password_hash`.
+    :param pepper:
+        A secret constant stored in the application code.
     :returns:
         `True` if the password matched, `False` otherwise.
 
@@ -90,10 +94,10 @@ def check_password_hash(password, pwhash):
         return False
 
     hashval, method, salt = pwhash.split('$', 2)
-    return hash_password(password, method, salt) == hashval
+    return hash_password(password, method, salt, pepper) == hashval
 
 
-def hash_password(password, method, salt):
+def hash_password(password, method, salt=None, pepper=None):
     """Hashes a password.
 
     Supports plaintext without salt, unsalted and salted passwords. In case
@@ -103,16 +107,16 @@ def hash_password(password, method, salt):
         The password to be hashed.
     :param method:
         A method from ``hashlib``, e.g., `sha1` or `md5`, or `plain`.
-    :paran salt:
+    :param salt:
         A random salt string.
+    :param pepper:
+        A secret constant stored in the application code.
     :returns:
         A hashed password.
 
     This function was ported and adapted from `Werkzeug`_.
     """
-    if isinstance(password, unicode):
-        password = password.encode('utf-8')
-
+    password = webapp2._to_utf8(password)
     if method == 'plain':
         return password
 
@@ -121,12 +125,11 @@ def hash_password(password, method, salt):
         return None
 
     if salt:
-        if isinstance(salt, unicode):
-            salt = salt.encode('utf-8')
-
-        h = hmac.new(salt, None, method)
+        h = hmac.new(webapp2._to_utf8(salt), password, method)
     else:
-        h = method()
+        h = method(password)
 
-    h.update(password)
+    if pepper:
+        h = hmac.new(webapp2._to_utf8(pepper), h.hexdigest(), method)
+
     return h.hexdigest()
