9031f474fa54b3ce59b882118e01cb3fd874e558
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-07-17 12:08:55 -0300

    And another.

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+import os
+import sys
 
-import sys, os
-
-on_rtd = os.environ.get('READTHEDOCS', None) is not None
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
