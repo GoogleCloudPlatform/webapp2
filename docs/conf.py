b0671cc0cd8a754d1cf117040e32d2072efed2b9
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-07-17 11:43:29 -0300

    Temporary docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -106,19 +106,15 @@ pygments_style = 'pygapp2.pygapp2'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'webapp2'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
-html_theme_options = {
-    'nosidebar': False,
-    'sidebarwidth': '200',
-}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_themes']
+#html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
