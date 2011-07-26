cb8550699dc31c5144b37f05acdd3f9d36a3b776
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-07-26 03:16:02 -0300

    Reset kind_map to avoid problems.

--- tests/extras_auth_test.py
+++ tests/extras_auth_test.py
@@ -1,7 +1,17 @@
+from experimental import auth
+from experimental.appengine.auth import models
+from experimental.appengine.ndb import unique_model
+
 import test_base
 
 class TestAuth(test_base.BaseTestCase):
-    pass
+
+    def setUp(self):
+        super(TestAuth, self).setUp()
+        self.register_model('User', models.User)
+        self.register_model('UserToken', models.UserToken)
+        self.register_model('Unique', unique_model.Unique)
+
 
 if __name__ == '__main__':
     test_base.main()
