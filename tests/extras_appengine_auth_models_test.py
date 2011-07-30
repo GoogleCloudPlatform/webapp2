03db1747ac4d5983ad3d979f261ebde348c68efa
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-07-30 05:19:25 -0300

    Moved Unique model to auth.models.

--- tests/extras_appengine_auth_models_test.py
+++ tests/extras_appengine_auth_models_test.py
@@ -1,8 +1,19 @@
 from webapp2_extras import auth
 from webapp2_extras.appengine.auth import models
-from webapp2_extras.appengine.ndb import unique_model
+
+from ndb import model
 
 import test_base
+
+
+class UniqueConstraintViolation(Exception):
+    pass
+
+
+class User(model.Model):
+    username = model.StringProperty(required=True)
+    auth_id = model.StringProperty()
+    email = model.StringProperty()
 
 
 class TestAuthModels(test_base.BaseTestCase):
@@ -11,7 +22,7 @@ class TestAuthModels(test_base.BaseTestCase):
         super(TestAuthModels, self).setUp()
         self.register_model('User', models.User)
         self.register_model('UserToken', models.UserToken)
-        self.register_model('Unique', unique_model.Unique)
+        self.register_model('Unique', models.Unique)
 
     def test_get(self):
         m = models.User
@@ -86,6 +97,81 @@ class TestAuthModels(test_base.BaseTestCase):
         self.assertTrue(m.validate_signup_token(auth_id, token))
         m.delete_signup_token(auth_id, token)
         self.assertFalse(m.validate_signup_token(auth_id, token))
+
+
+class TestUniqueModel(test_base.BaseTestCase):
+    def setUp(self):
+        super(TestUniqueModel, self).setUp()
+        self.register_model('Unique', models.Unique)
+
+    def test_single(self):
+        def create_user(username):
+            # Assemble the unique scope/value combinations.
+            unique_username = 'User.username:%s' % username
+
+            # Create the unique username, auth_id and email.
+            success = models.Unique.create(unique_username)
+
+            if success:
+                user = User(username=username)
+                user.put()
+                return user
+            else:
+                raise UniqueConstraintViolation('Username %s already '
+                    'exists' % username)
+
+        user = create_user('username_1')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_1')
+
+        user = create_user('username_2')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_2')
+
+    def test_multi(self):
+        def create_user(username, auth_id, email):
+            # Assemble the unique scope/value combinations.
+            unique_username = 'User.username:%s' % username
+            unique_auth_id = 'User.auth_id:%s' % auth_id
+            unique_email = 'User.email:%s' % email
+
+            # Create the unique username, auth_id and email.
+            uniques = [unique_username, unique_auth_id, unique_email]
+            success, existing = models.Unique.create_multi(uniques)
+
+            if success:
+                user = User(username=username, auth_id=auth_id, email=email)
+                user.put()
+                return user
+            else:
+                if unique_username in existing:
+                    raise UniqueConstraintViolation('Username %s already '
+                        'exists' % username)
+                if unique_auth_id in existing:
+                    raise UniqueConstraintViolation('Auth id %s already '
+                        'exists' % auth_id)
+                if unique_email in existing:
+                    raise UniqueConstraintViolation('Email %s already '
+                        'exists' % email)
+
+        user = create_user('username_1', 'auth_id_1', 'email_1')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_1', 'auth_id_2', 'email_2')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_2', 'auth_id_1', 'email_2')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_2', 'auth_id_2', 'email_1')
+
+        user = create_user('username_2', 'auth_id_2', 'email_2')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_2', 'auth_id_1', 'email_1')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_1', 'auth_id_2', 'email_1')
+        self.assertRaises(UniqueConstraintViolation, create_user, 'username_1', 'auth_id_1', 'email_2')
+
+    def test_delete_multi(self):
+        rv = models.Unique.create_multi(('foo', 'bar', 'baz'))
+        self.assertEqual(rv, (True, []))
+        rv = models.Unique.create_multi(('foo', 'bar', 'baz'))
+        self.assertEqual(rv, (False, ['foo', 'bar', 'baz']))
+
+        models.Unique.delete_multi(('foo', 'bar', 'baz'))
+
+        rv = models.Unique.create_multi(('foo', 'bar', 'baz'))
+        self.assertEqual(rv, (True, []))
 
 
 if __name__ == '__main__':
