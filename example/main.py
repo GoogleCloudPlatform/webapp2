3317eb8a4d9b20d54bfdb8587756798cbbf3fe00
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2016-04-05 11:30:24 -0700

    Cleaning up example

--- example/main.py
+++ example/main.py
@@ -1,3 +1,17 @@
+# Copyright 2015 webapp2 AUTHORS.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import webapp2
 
 
@@ -30,9 +44,16 @@ app = webapp2.WSGIApplication([
     # Loads a handler lazily.
     webapp2.Route('/lazy', 'handlers.LazyHandler', name='lazy'),
     # Redirects to a given path.
-    webapp2.Route('/redirect-me', webapp2.RedirectHandler, defaults={'url': '/lazy'}),
+    webapp2.Route(
+        '/redirect-me',
+        webapp2.RedirectHandler,
+        defaults={'url': '/lazy'}),
     # Redirects to a URL using a callable to get the destination URL.
-    webapp2.Route('/redirect-me2', webapp2.RedirectHandler, defaults={'url': get_redirect_url}),
-    # No exception should pass. If exceptions are not handled, a 500 page is displayed.
+    webapp2.Route(
+        '/redirect-me2',
+        webapp2.RedirectHandler,
+        defaults={'url': get_redirect_url}),
+    # No exception should pass. If exceptions are not handled, a 500 page is
+    # displayed.
     webapp2.Route('/exception', HandlerWithError),
 ])
